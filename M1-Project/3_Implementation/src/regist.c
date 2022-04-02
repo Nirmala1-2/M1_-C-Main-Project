37e775b02f840df6876a2317b588c2ba1cdf7101
Nirmala1-2 <101582230+Nirmala1-2@users.noreply.github.com>
2022-04-02 03:07:38 -0600

    Update regist.c

--- M1-Project/3_Implementation/src/regist.c
+++ M1-Project/3_Implementation/src/regist.c
@@ -1,1 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+ switch(choose)
+
+      {
+        case 1:
+
+         {
+             printf("\t \t \t 1) citizen card");
+             break;
+          }
+
+   
+        case 2:
+
+       {
+           printf("\t \t \t 2) search your profile");
+           break;
+         }
+
+        case 3:
+
+       {
+           printf("\t \t \t 3) others");
+           break;
+        }
+
+        default:
+
+       {
+           printf("\n enter correct choice");
+           break;
+        }
+
+    }
+
+    break;
+}
