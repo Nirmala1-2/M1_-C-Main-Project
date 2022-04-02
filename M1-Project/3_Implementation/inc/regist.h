94399ca278e5a9ede26f6adfc4ac5c2197a1dc45
Nirmala1-2 <101582230+Nirmala1-2@users.noreply.github.com>
2022-04-02 14:19:59 +0530

    Add files via upload

---
+++ M1-Project/3_Implementation/inc/regist.h
@@ -0,0 +1,27 @@
+#ifndef REGIST_C
+#define REGIST_C
+#include<stdio.h>
+#include<stdlib.h>
+
+
+ // Checks for registration details of new patients
+int regist_status(long pd );
+
+
+// Checks details from the pre registered patients
+int existregist();
+
+
+// Checks how many doses should be given
+int vaccine_status(int );
+
+
+
+void main {
+    int citizen card;
+    int student card;
+    int ph_number;
+    int secret_code;
+   }
+
+#endif
