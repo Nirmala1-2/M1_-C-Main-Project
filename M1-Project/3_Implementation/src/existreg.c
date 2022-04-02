8963b1d9b75f5ddca5e3f1d73bf79af8559d687e
Nirmala1-2 <101582230+Nirmala1-2@users.noreply.github.com>
2022-04-02 03:01:30 -0600

    Update existreg.c

--- M1-Project/3_Implementation/src/existreg.c
+++ M1-Project/3_Implementation/src/existreg.c
@@ -1,1 +1,13 @@
+#include"stdio.h"
+#include "stdlib.h"
 
+// Patients already registered list is given as input for verification
+// pd stands for patient details
+
+int existregist(){
+    pd[1].age=123;
+    pd[1].citizen card _number=123;
+    pd[1].student card_number=123;
+    pd[1].secret_code=123;
+    return 0;
+}
